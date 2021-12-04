d753826c7c39911eae01600449d75e4184a00220
gumulaola <fx96@stu.pku.edu.cn>
2021-12-04 18:14:35 +0800

    select k

---
+++ Lecture4/select.py
@@ -0,0 +1,35 @@
+nums = [7, 6, 0, 5, 3, 1, 2, 9, 8]
+k = 4
+
+
+def select(nums, k):
+    p = getPivot(nums)
+    left, right, pivot = partition(nums, p)
+    if k == len(left) + 1:
+        return pivot
+    elif k < len(left) + 1:
+        return select(left, k)
+    else:
+        return select(right, k-len(left)-1)
+
+
+def getPivot(nums):
+    return round(len(nums)/2)
+
+
+def partition(nums, p):
+    pivot = nums[p]
+    left = []
+    right = []
+    for i in range(len(nums)):
+        if i == p:
+            continue
+        else:
+            if nums[i] <= nums[p]:
+                left.append(nums[i])
+            else:
+                right.append(nums[i])
+    return left, right, pivot
+
+
+print(select(nums, k))
